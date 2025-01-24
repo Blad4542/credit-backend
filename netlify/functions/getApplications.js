05cd550aafee1b9c88ed14028142ef3c57b569da
Peniche <josecar366@gmail.com>
2025-01-23 22:30:52 -0600

    adding pagination

--- netlify/functions/getApplications.js
+++ netlify/functions/getApplications.js
@@ -14,14 +14,42 @@ exports.handler = async (event) => {
     };
   }
 
+  if (event.httpMethod !== "GET") {
+    return {
+      statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      },
+      body: JSON.stringify({ error: "Method not allowed" }),
+    };
+  }
+
   try {
     logger.info("Starting getApplications function...");
     logger.info("Fetching data from Firestore collection: creditApplications");
 
-    const snapshot = await db.collection("creditApplications").get();
+    // Obtén los parámetros de paginación de la query string
+    const { page = 1, limit = 15 } = event.queryStringParameters || {};
+    const pageNumber = parseInt(page, 10);
+    const pageSize = parseInt(limit, 10);
+    const offset = (pageNumber - 1) * pageSize;
+
+    // Obtén el total de documentos
+    const totalSnapshot = await db.collection("creditApplications").get();
+    const totalRecords = totalSnapshot.size;
+
+    // Obtén los documentos para la página actual
+    const snapshot = await db
+      .collection("creditApplications")
+      .orderBy("createdAt") // Ordena por fecha de creación
+      .offset(offset)
+      .limit(pageSize)
+      .get();
 
     if (snapshot.empty) {
-      logger.warn("No applications found in the Firestore collection.");
+      logger.warn("No applications found for the current page.");
       return {
         statusCode: 200,
         headers: {
@@ -29,7 +57,7 @@ exports.handler = async (event) => {
           "Access-Control-Allow-Headers": "Content-Type",
           "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         },
-        body: JSON.stringify([]),
+        body: JSON.stringify({ data: [], totalRecords }),
       };
     }
 
@@ -47,7 +75,7 @@ exports.handler = async (event) => {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       },
-      body: JSON.stringify(applications),
+      body: JSON.stringify({ data: applications, totalRecords }),
     };
   } catch (error) {
     logger.error(`Error getting the applications: ${error}`);
